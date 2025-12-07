0556776a8fdf6304c9e7c11e6f97af53a569497b
luisetex <luis@seedtag.com>
2025-12-07 14:03:47 +0100

    07 B

--- 07.py
+++ 07.py
@@ -22,9 +22,9 @@ def split_beam(
     return splits, is_split
 
 
-start_index = diagram[0].index("S")
+start_column = diagram[0].index("S")
 beams: set[tuple[int, int]] = set()
-beams.add((1, start_index))
+beams.add((1, start_column))
 
 total_splits = 0
 for row in diagram[1:-1:2]:
@@ -38,3 +38,42 @@ for row in diagram[1:-1:2]:
 print(total_splits)
 
 # B
+
+
+def find_timelines(beam_index: tuple[int, int], grid, cache: dict[str, int]):
+    timelines = 0
+    found = False
+    for next_row_index in range(beam_index[0] + 1, len(grid)):
+        if grid[next_row_index][beam_index[1]] == ".":
+            continue
+        left = next_row_index, beam_index[1] - 1
+        right = next_row_index, beam_index[1] + 1
+        left_key = str(left[0]) + "-" + str(left[1])
+        right_key = str(right[0]) + "-" + str(right[1])
+        if 0 < beam_index[1] - 1 < len(grid[0]):
+            if left_key in cache:
+                timelines += cache[left_key]
+            else:
+                found_timelines = find_timelines(left, grid, cache)
+                cache[left_key] = found_timelines
+                timelines += found_timelines
+            found = True
+        if 0 < beam_index[1] + 1 < len(grid[0]):
+            if right_key in cache:
+                timelines += cache[right_key]
+            else:
+                found_timelines = find_timelines(right, grid, cache)
+                cache[right_key] = found_timelines
+                timelines += found_timelines
+            found = True
+        if found:
+            break
+    if next_row_index == len(grid) - 1:
+        timelines += 1
+    return timelines
+
+
+start_column = diagram[0].index("S")
+start_index = (1, start_column)
+
+print(find_timelines(start_index, diagram, {}) + 1)
